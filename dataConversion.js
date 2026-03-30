ce18b7678d4b00efc4ae0a639768d41085c66ba9
niteshtechdev-blip <nitesh.tech.dev@gmail.com>
2026-03-30 13:11:29 +0000

    dataConversion

---
+++ dataConversion.js
@@ -0,0 +1,13 @@
+console.log("hello")
+let name="Nitesh"
+let age=18
+let isAdult=true
+console.table([name,age,isAdult])
+
+let num=undefined
+let newNum=Number(num)
+console.log(newNum)
+console.log(typeof newNum)
+let isAdult1= NaN
+let boolIsAdult=Boolean(isAdult1)
+console.log(boolIsAdult)
